ba12f47c7b3bdb99943667d1bdc5197d065be758
Pat <proach@gap.com>
2019-06-12 12:37:51 -0400

    Reorg tests, adding inputs for questions

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
          Tiebreaker
         </p>
+        <input>q</input>
       </header>
     </div>
   );
