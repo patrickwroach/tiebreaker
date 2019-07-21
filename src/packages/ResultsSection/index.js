1110a01d62f7a0152c04b3aae4e60f982621dbe4
patrickwroach <patrickwroach@gmail.com>
2019-07-21 17:39:00 -0400

    Randomizer inline vis test

--- src/packages/ResultsSection/index.js
+++ src/packages/ResultsSection/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Randomizer from 'react-randomizer';
  
 class ResultsSection extends Component {
  render() {
     return (
       <div className="results-section">
           <h1>Player 1 is the winner y'all!</h1>
+          <p>Please enjoy this random number:</p>
+          <p>{Randomizer.randomNumber(1,100)}</p>
       </div>
     )
   }
