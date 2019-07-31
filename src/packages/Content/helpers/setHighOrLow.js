5048dc7239a19098cba2703549a4ce1baf4e513d
Pat <proach@gap.com>
2019-07-31 10:49:28 -0400

    Cleaning up setHighOrLow

--- src/packages/Content/helpers/setHighOrLow.js
+++ src/packages/Content/helpers/setHighOrLow.js
@@ -1,7 +1,9 @@
+
 import Randomizer from "react-randomizer";
 
 export function setHighOrLow() {
-  let randomizedHighWins = () => Randomizer.randomNumber(0, 1) > 0 ? true : false;
+  let randomizedHighWins = Randomizer.randomNumber(0, 1) > 0;
+  console.log(randomizedHighWins)
   this.setState({
     highWins: randomizedHighWins
   });
